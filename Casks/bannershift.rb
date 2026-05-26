225575a98fdfaa6ee7204baf074786d3d6f3dd98
Ryan Emerle <ryan@emerle.net>
2026-05-26 12:20:22 -0400

    Satisfy brew style on bannershift cask
    - Drop 'macOS' from desc (Cask/Desc: platform is implicit for casks)
    - Use :ventura symbol instead of ">= :ventura" string (Homebrew/OSDependsOn:
      the bare symbol already means 'ventura or newer')
    - Alphabetize zap trash entries (Cask/ArrayAlphabetization)

--- Casks/bannershift.rb
+++ Casks/bannershift.rb
@@ -5,7 +5,7 @@ cask "bannershift" do
   url "https://github.com/remerle/BannerShift/releases/download/v#{version}/BannerShift-#{version}.dmg",
       verified: "github.com/remerle/BannerShift/"
   name "BannerShift"
-  desc "Repositions macOS notification banners onto a 3x3 grid"
+  desc "Repositions notification banners onto a 3x3 grid"
   homepage "https://github.com/remerle/BannerShift"
 
   livecheck do
@@ -13,17 +13,17 @@ cask "bannershift" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "BannerShift.app"
 
   uninstall quit: "com.emerle.BannerShift"
 
   zap trash: [
-    "~/Library/Logs/BannerShift.log",
-    "~/Library/Preferences/com.emerle.BannerShift.plist",
     "~/Library/Caches/com.emerle.BannerShift",
     "~/Library/HTTPStorages/com.emerle.BannerShift",
+    "~/Library/Logs/BannerShift.log",
+    "~/Library/Preferences/com.emerle.BannerShift.plist",
     "~/Library/Saved Application State/com.emerle.BannerShift.savedState",
   ]
 end
